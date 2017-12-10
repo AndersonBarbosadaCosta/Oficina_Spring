d898c1563c98ab203b07ceae2259cbfc452cd46e
Anderson Costa <andersonb.costa2@gmail.com>
2017-12-10 13:44:38 -0200

    Termino da validacao e criacao de arquivos de propriedade

--- src/main/java/com/projetos/vinhos/controller/VinhosController.java
+++ src/main/java/com/projetos/vinhos/controller/VinhosController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.projetos.vinhos.model.TipoVinho;
 import com.projetos.vinhos.model.Vinho;
@@ -36,12 +37,13 @@ public class VinhosController {
 	}
 
 	@PostMapping("/novo")
-	public ModelAndView salvar(@Valid Vinho vinho, BindingResult result) {
+	public ModelAndView salvar(@Valid Vinho vinho, BindingResult result,RedirectAttributes attributes) {
 
 		if (result.hasErrors()) {
 			return novo(vinho);
 		}
 		this.vinhos.save(vinho);
+		attributes.addFlashAttribute("mensagem","Vinho Salvo com Sucesso !!");
 		return new ModelAndView("redirect:/vinhos/novo");
 	}
 }
